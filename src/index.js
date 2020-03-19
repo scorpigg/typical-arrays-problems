73eaa7d527c196e20574e619939e4738d4572551
Andrey Nikitin <scoprigg@yandex.ru>
2020-03-19 22:13:49 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+    if(arr === undefined || arr.length == 0){
+        return 0;
+    }else{
+        let min = arr[0];
+
+        for (let i = 0; i < arr.length; i++) {
+            if(min >= arr[i]){
+                min = arr[i];
+            }
+        }
+         return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+    if(arr === undefined || arr.length == 0){
+        return 0;
+    }else{
+        let max = arr[0];
+
+        for (let i = 0; i < arr.length; i++) {
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+         return max;
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+    if(arr === undefined || arr.length == 0){
+        return 0;
+    }else{
+        let avg = 0;
+
+        for (let i = 0; i < arr.length; i++) {
+            avg += arr[i];
+        }
+         return avg/arr.length;
+    }
 }
